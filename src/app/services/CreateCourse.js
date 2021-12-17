95f8155b663b87969314b31a407a5a0ba18a6987
Atomics <info@atomicsdigital.com>
2021-12-17 18:55:59 -0500

    create course set merge fix

--- src/app/services/CreateCourse.js
+++ src/app/services/CreateCourse.js
@@ -38,24 +38,22 @@ export const saveCourse = (courseID, lessons, courseObject) => {
   return updateDB('courses', courseID, courseObject).then(() => {
     lessons.forEach(lesson => {
       const docRef = db.collection('courses').doc(courseID).collection('lessons').doc(lesson?.lessonID)
-      batch.delete(docRef)
+      batch.set(docRef, {lessonID: lesson?.lessonID, lessonType: 'video', title: lesson?.title, videoType: lesson?.videoType}, {merge: true})
     })
     lessons.forEach(lesson => {
       const docRef = db.collection('courses').doc(courseID).collection('lessons').doc(lesson?.lessonID)
-      batch.set(docRef, {lessonID: lesson?.lessonID, lessonType: 'video', title: lesson?.title, videoType: lesson?.videoType})
+      batch.set(docRef, {lessonID: lesson?.lessonID, lessonType: 'video', title: lesson?.title, videoType: lesson?.videoType}, {merge: true})
     })
     lessons.forEach(lesson => {
       lesson.videos.forEach(video => {
         const docRef = db.collection('courses').doc(courseID).collection('lessons').doc(lesson?.lessonID).collection('videos').doc(video?.videoID)
-        batch.set(docRef, {
-          videoID: video?.videoID, title: video?.title, duration: video?.duration, url: video?.url, dateAdded: new Date()
-        })
+        batch.set(docRef, {videoID: video?.videoID, title: video?.title, duration: video?.duration, url: video?.url, dateAdded: new Date()}, {merge: true})
       })
     })
     lessons.forEach(lesson => {
       lesson.notes.forEach(note => {
         const docRef = db.collection('courses').doc(courseID).collection('lessons').doc(lesson?.lessonID).collection('notes').doc(note?.noteID)
-        batch.set(docRef, {noteID: note?.noteID, text: note?.text, title: note?.title, dateAdded: new Date()})
+        batch.set(docRef, {noteID: note?.noteID, text: note?.text, title: note?.title, dateAdded: new Date()}, {merge: true})
       })
     })
     return batch.commit()
