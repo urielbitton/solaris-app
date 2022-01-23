b87c21e39f1e6ee021fd41f67e5fc8f44803ba6e
Atomics <info@atomicsdigital.com>
2022-01-23 00:20:30 -0500

    sdk test 1

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
+
 const algoliasearch = require('algoliasearch')
 
 const APP_ID = functions.config().algolia.app
@@ -25,4 +27,12 @@ exports.updateIndex = functions.firestore.document('courses/{courseID}').onUpdat
 
 exports.deleteFromIndex = functions.firestore.document('courses/{courseID}').onDelete(snapshot => {
   index.deleteObject(snapshot.id)
+})
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+      projectId: "solaris-app-22479",
+      privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC9C5JlFFAHMagF\nr/b9NRHIKCI7r1rFzpfX2X5N91N3p3mLOwcA78SRT9hN5VepAMCW+9M+1d1F1kyN\nb7ALXmPZAyHrO5HKg1cq2DG+vb72TcPudJPilsqden1yOb3hpB+4txFFGaf3BDjV\n/RvDNkcOIQRAiHVZr/eYWb2VGq71iPvAmRSh1a8YhzWcTtf46dXVa9qMnTWGRNmm\nY8uWljPCl694lsc9PgebwhH3GyNA5NKPH60Es35lJBPSL1WkqdM9BtIdLWXs4PFV\nicBp7NZjgU8mh3GG8bEZ5XPBgIyKjN7rXXCzUSvZl8r6uMw+ZOlCU4QGr2DDWMJ5\nmXBkAweHAgMBAAECggEAH2iTR5Dy5OcWo+9C6XnOPQOSAHmXasLyzm13wyti5j/g\nfUXu2XR8o6nw5axDfrQ9yzgGtmtg6o/6wwr55EGn9C/FjGgmxk9BBcDKrwamehSc\nh3s98Mkd28E0o2w9hIY4sP9VcrYbBnrgmIkHek0DzDCnHLOvT5nhWKIcUfAPfoKc\nqy3CcliHYHHSnyHxw3/sCeuTRtyw7s/KUKaetlg7Bm5HsJhm7qsaY7cvJex1jHiJ\nHOxyQQ+A29JoW8khPKhp4fdryMLxCT8X6gEPWZOT+MGIWg+OEFuvrLvRVY7Us4ds\nHi5NgwzEKiI0p9EfWM83La8Q7ZRybnnmKgdKxNClDQKBgQD5Dpzqy4s2fKFDI+Xk\ns1fME+kRfAEZe+y13TG0yGAkOgFKHYcvQRyK/BTN4mlTpCSzu85Td1b5ApJuIzIC\nq01Lkv+prp4nzAyaa7+opT+nD5v9u+iMoEtfF8YS43DDAmFN+nYz+36wRCCurtxV\nXfE8cr4ALQcbar7uZoSyPAJ94wKBgQDCUK+t1lys2eRk1EEZ++fvT0eggoKCxZe+\n9c80cSfi9u3suTVvHvwvZ+4u4hKhTxdtPFoaftNWA68RKOUDXuPt2JKbmK+fRX3r\nv2NLUySzvu4AuXGS3HmsDraBeuxopW1AqP8hshE0himihTW4Xk4Ux3GHXJffJTiz\ndGTuliRBDQKBgQCxF3rsWR7791KTcwscu4lKSz4GeK4veLav+kBF09TI7ZXNJYHi\nT+ID0YrZeox6NNUEGB51qv3kwbdtKQl+bF/hxPyVotxCYAz2IHd/a0D/gVHh8aNB\nmxj83OKyu5aKkz6tO9Kg08sZetfiXeO9r7bAzLXdWicbj6OD0NG6CoGbEQKBgGZp\nuj3n5g5gBOUzjWBRiUtywHCTeI/N7o2B5oRgQ0J3HK0QbF41uXqOz1U8Z89G7TTw\ngxjF0ULbTDDCGfmt0HmyFIsXbTCHordWttGjb4ugzLXrBzX9J8+MLZqOm/0He06C\nA33R6QmC0HzMsqSKZ6LB/071eUHtzzWd2fh5f4AdAoGAGbEt+Nq0s1l1UoSUXwBl\nLpsQufyFf65FHsoO1uLaXEU5bjWS0EEn5wOqiOa1SoqS1q+ZT5YbS/2cAQ+Y+YzZ\nLGTxRfx5T6F+bEgfbqlQPtx8AeDfH9F3RGx5QW/qawrgFhHqFqyOntIsM1iF6Tz5\nCpfqzUyAhawXdKk7h1QZhA4=\n-----END PRIVATE KEY-----\n".replace(/\\n/g, '\n'),
+      clientEmail: "firebase-adminsdk-wviey@solaris-app-22479.iam.gserviceaccount.com",
+  })
 })
--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -8,6 +8,7 @@ import { getTopRatedInstructors } from "../services/InstructorServices"
 import InstructorCard from '../components/instructor/InstructorCard'
 import { getCoursesIDEnrolledByUserID } from "../services/userServices"
 import onlineLearningImg from '../assets/imgs/online-learning.png'
+import admin from "firebase-admin"
 
 export default function Home() {
 
@@ -37,10 +38,21 @@ export default function Home() {
     setNavDescript('Welcome ' + user?.displayName?.split(' ')[0])
   },[]) 
 
+  const sendEmail = () => {
+    admin.firestore().collection('mail').doc('newEmail').set({
+      to: 'urielas@hotmail.com',
+      message: {
+        subject: 'Hello from Firebase!',
+        html: 'This is an <code>HTML</code> email body.',
+      }
+    }).then((res) => console.log(res))
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="home-page">
-      <section className="intro">
-        <div>
+      <section className="intro" onClick={() => sendEmail()}>
+        <div> 
           <h4>Hi {user?.displayName}</h4>
           <h6>{new Date().toDateString('en-CA', {weekday: 'long', month: 'long'})}</h6>
           <span>You are enrolled in {coursesEnrolled.length} course{coursesEnrolled.length !== 1 ? "s" : ""}</span>
