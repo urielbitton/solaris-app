42f158fc7ad5d48032b3c6cd0cb55c746b38c9bf
Atomics <info@atomicsdigital.com>
2022-02-05 11:49:03 -0500

    responsive fixes 1

--- src/app/containers/AppContainer.js
+++ src/app/containers/AppContainer.js
@@ -1,15 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import HomeCont from './HomeCont'
 import Sidebar from '../components/layout/Sidebar'
 import './styles/AppContainer.css'
 import './styles/DarkMode.css'
 import { InstantSearch } from 'react-instantsearch-dom';
 import { searchClient } from '../algolia/index'
-import { StoreContext } from "../store/store";
+import { StoreContext } from "../store/store"
+import { useWindowDimensions } from '../utils/customHooks'
 
 export default function AppContainer() {
 
-  const { darkMode } = useContext(StoreContext)
+  const { darkMode, setWindowPadding } = useContext(StoreContext)
+  const { screenWidth } = useWindowDimensions()
+
+  useEffect(() => {
+    if(screenWidth < 600) {
+      setWindowPadding('100px 15px 0 15px')
+    }
+    else {
+      setWindowPadding('100px 22px 0 22px')
+    }
+  },[screenWidth])
 
   return (
     <div className={`app-container ${ darkMode ? "dark-app" : "" }`}>
